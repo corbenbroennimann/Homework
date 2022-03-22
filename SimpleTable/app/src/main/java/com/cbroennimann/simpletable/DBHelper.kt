134ca7e3126bab115ce91d2c9dd3256a5353852b
Corben Broennimann <corben@hawaii.edu>
2022-03-21 22:01:32 -1000

    Assignment 21 Mar

---
+++ SimpleTable/app/src/main/java/com/cbroennimann/simpletable/DBHelper.kt
@@ -0,0 +1,97 @@
+package com.cbroennimann.simpletable
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+import java.io.IOException
+import java.lang.Exception
+
+
+class DBHelper (context: Context, factory: SQLiteDatabase.CursorFactory?) :
+    SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION){
+
+    override fun onCreate(db: SQLiteDatabase) {
+        val query = ("CREATE TABLE " + TABLE_NAME + " ("
+                + UNIQUE_ID_COL + " INT, " +
+                FIRST_NAME_COL + " TEXT," +
+                LAST_NAME_COL + " TEXT," +
+                REWARDS_COL + " INT" + ")")
+        db.execSQL(query)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, p1: Int, p2: Int) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME)
+        onCreate(db)
+    }
+
+    fun addMember(id: Int,fstName: String, lstName: String, rewards: Int){
+        val values = ContentValues()
+
+
+        values.put(UNIQUE_ID_COL,id)
+        values.put(FIRST_NAME_COL,fstName)
+        values.put(LAST_NAME_COL,lstName)
+        values.put(REWARDS_COL, rewards)
+
+        val db = this.writableDatabase
+        checkMember(id)
+
+        if(checkMember(id)){
+            //db.update(TABLE_NAME, values, "$UNIQUE_ID_COL=1", arrayOf())
+            deleteMember(id)
+
+        }
+            db.insert(TABLE_NAME, null, values)
+
+
+
+
+        db.close()
+    }
+
+    fun getMember(id: Int) : Cursor{
+            val db = this.readableDatabase
+            return db.rawQuery(
+                "SELECT * FROM " + TABLE_NAME + " WHERE " + UNIQUE_ID_COL + " = " + id,
+                null
+            )
+    }
+
+    fun deleteMember(id: Int?) {
+            if (id != null) {
+                val db = this.readableDatabase
+                db.delete(TABLE_NAME, UNIQUE_ID_COL + " = " + id, null)
+                //db.execSQL("DELETE FROM " + TABLE_NAME + " WHERE " + UNIQUE_ID_COL + " = " + id, null)
+            }
+    }
+
+    fun checkMember(id: Int) : Boolean {
+        val db = this.readableDatabase
+        var bool = false
+        val cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + UNIQUE_ID_COL + " = " + id, null)
+        var list: MutableList<Int> = arrayListOf()
+
+        if(cursor.columnCount > 1) {
+            bool = true
+            Log.d("TAG", "Cursor is not null")
+        }
+        //cursor!!.moveToFirst()
+        //list.add(cursor.getInt(cursor.getColumnIndex()))
+        return bool
+    }
+
+
+    companion object{
+        private val DATABASE_NAME = "SIMPLETABLE"
+        private val DATABASE_VERSION = 1
+        val TABLE_NAME = "simple_table"
+        val UNIQUE_ID_COL = "id"
+        val FIRST_NAME_COL = "first_name"
+        val LAST_NAME_COL = "last_name"
+        val REWARDS_COL = "rewards"
+    }
+
+}
---
+++ SimpleTable/app/src/main/java/com/cbroennimann/simpletable/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.cbroennimann.simpletable
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.cbroennimann.simpletable.databinding.ActivityMainBinding
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("Range")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addMember.setOnClickListener {
+            val db = DBHelper(this, null)
+            val id = binding.enterUniqueID.text.toString().toInt()
+            val firstName = binding.enterFirstName.text.toString()
+            val lastName = binding.enterLastName.text.toString()
+            val reward = binding.enterRewards.text.toString().toInt()
+
+            db.addMember(id,firstName,lastName,reward)
+
+            Toast.makeText(this, id.toString() + " added to database", Toast.LENGTH_LONG).show()
+
+            binding.enterUniqueID.text.clear()
+            binding.enterFirstName.text.clear()
+            binding.enterLastName.text.clear()
+            binding.enterRewards.text.clear()
+        }
+
+        binding.deleteMember.setOnClickListener {
+            val db = DBHelper(this, null)
+            try {
+                db.deleteMember(binding.enterID.text.toString().toInt())
+                binding.enterID.text.clear()
+            } catch(e: Exception) {
+
+            }
+        }
+
+        binding.displayInfo.setOnClickListener {
+            val db = DBHelper(this, null)
+            try {
+                if (binding.enterID.text.toString() != null) {
+                    val cursor = db.getMember(binding.enterID.text.toString().toInt())
+
+                    cursor!!.moveToFirst()
+                    binding.PrimeKey.setText(
+                        cursor.getInt(cursor.getColumnIndex(DBHelper.UNIQUE_ID_COL))
+                            .toString() + "\n"
+                    )
+                    binding.fName.setText(cursor.getString(cursor.getColumnIndex(DBHelper.FIRST_NAME_COL)) + "\n")
+                    binding.lName.setText(cursor.getString(cursor.getColumnIndex(DBHelper.LAST_NAME_COL)) + "\n")
+                    binding.reward.setText(cursor.getString(cursor.getColumnIndex(DBHelper.REWARDS_COL)) + "\n")
+
+                    binding.enterID.text.clear()
+                    cursor.close()
+                }
+            }catch(e:Exception) {
+
+            }
+        }
+
+
+
+    }
+}
