b0369d26e9134b61260c339edfe053ba3ee190e7
Corben Broennimann <corben@hawaii.edu>
2022-04-07 12:49:26 -1000

    imperial change

--- Weather/app/src/main/java/com/cbroennimann/weather/api/WeatherApi.kt
+++ Weather/app/src/main/java/com/cbroennimann/weather/api/WeatherApi.kt
@@ -7,7 +7,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherApi {
-    @GET("data/2.5/weather?&units=metric&APPID=API_KEY_HERE")
+    @GET("data/2.5/weather?&units=imperial&APPID=API_KEY_HERE")
         fun getData(
         @Query("q") cityName: String
     ):  Single<WeatherData>
